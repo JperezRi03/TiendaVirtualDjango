796cc3f8626e82463b07d2ea92cc6788c8cbb4ab
JuanPerez <juan.perez@lgcns.com>
2025-07-03 10:26:52 -0500

    Se añadiio la logica con la libreria uuid para la generacion de slugs diferente por si hay productos con un mismo id

--- products/models.py
+++ products/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
+import uuid 
 
 # Create your models here.
 
@@ -8,6 +11,17 @@ class Product(models.Model):
     price= models.DecimalField(max_digits=8, decimal_places=2, default=0.0) # type: ignore
     created_at = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=200, null=False, blank=False, unique=True)
-
+    
     def __str__(self):
         return self.title
+
+def new_slug(sender , instance, *args , **kwargs):
+        if instance.title and not instance.slug:
+            slug = slugify(instance.title)
+            while Product.objects.filter(slug=slug).exists():
+                 slug = slugify(
+                      '{}-{}'.format(instance.title , str(uuid.uuid4())[:8] 
+                                     ))
+            instance.slug=slug
+
+pre_save.connect(new_slug, sender=Product)
