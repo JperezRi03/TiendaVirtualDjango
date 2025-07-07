9c0266ebd73b78747dd0dda169dae16ef6f2ade7
JperezRi03 <juan.perez@lgcns.com>
2025-07-07 14:10:35 -0500

    Calcular Totales y subtotal, metodos para contar cantidad implementados, pendiente actualizar meotodos para los calculos de subtotal

--- carts/models.py
+++ carts/models.py
@@ -1,24 +1,51 @@
 from django.db import models
 from users.models import User
-from django.db.models.deletion import CASCADE
 from products.models import Product
-from django.db.models.signals import pre_save
-import uuid 
+from django.db.models.signals import pre_save, m2m_changed
+import uuid , decimal
 # Create your models here.
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
+    products = models.ManyToManyField(Product, through='CartProduct')
     subtotal = models.DecimalField(default=0.0, max_digits=8, decimal_places=2) # type: ignore
     total = models.DecimalField(default=0.0, max_digits=8, decimal_places=2) # type: ignore
     created_at = models.DateTimeField(auto_now_add=True)
+    FEE = 0.01
 
     def __str__(self): # type: ignore
         return self.cart_id
     
+    def update_totals(self):
+        self.update_subtotal()
+        self.update_total()
+
+    def update_subtotal(self):
+        self.subtotal = sum([product.price for product in self.products.all()])
+        self.save()
+
+    def update_total(self):
+        self.total = self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE))
+        self.save()
+
+    def product_related(self):
+        return self.cartproduct_set.select_related('product') # type: ignore
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default = 1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 def set_cart_id(sender,instance, *args,**kwargs):
     if not instance.cart_id:
         instance.cart_id = str(uuid.uuid4())
 
+
+def update_totals(sender, instance, action , *args, **kwargs ):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        instance.update_totals()
+
 pre_save.connect(set_cart_id, sender=Cart)
+#m2m_changed.connect(update_totals, sender=Cart.products.through)
