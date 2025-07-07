8ab9d40dc3b64bb69cc0065db28f34335bbe83da
JperezRi03 <juan.perez@lgcns.com>
2025-07-07 10:48:06 -0500

    Vista carrito de compra con funcionalidad para agregar y eliminar productos del carrito.

--- carts/views.py
+++ carts/views.py
@@ -1,20 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect , get_object_or_404
 from . import views
 from .models import Cart
 from products.models import Product
 from .funciones import funcionesCarrito
 
+
 def cart(request):
     cart = funcionesCarrito(request)
-    return render(request, 'carts/cart.html', {})
+    return render(request, 'carts/cart.html', {
+        'cart' : cart
+    })
 # Create your views here.
 
 
 def add(request):
     cart = funcionesCarrito(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
 
     cart.products.add(product)
     return render(request, 'carts/add.html',{
         'product': product 
     })
+
+def remove(request):
+    cart = funcionesCarrito(request)
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
+    cart.products.remove(product)
+
+    return redirect('cart')
